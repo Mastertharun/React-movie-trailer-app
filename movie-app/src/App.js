75db96ff60ed51b8cf261a4dfa2711b82e7debaf
Tharun013 <155997984+Mastertharun@users.noreply.github.com>
2025-02-20 19:12:24 +0530

    App.js

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,10 +3,8 @@ import axios from "axios";
 import YouTube from "react-youtube";
 import "./App.css";
 
-const TMDB_API_KEY = "e6fcd25d55477bb3d5d3e4cedfe19bfe";
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+const API_KEY = "e6fcd25d55477bb3d5d3e4cedfe19bfe";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -20,44 +18,22 @@ const App = () => {
 
   const fetchMovies = async (query) => {
     const url = query === "popular"
-      ? `${TMDB_BASE_URL}/movie/popular`
-      : `${TMDB_BASE_URL}/search/movie`;
-
+      ? `${BASE_URL}/movie/popular`
+      : `${BASE_URL}/search/movie`;
+    
     const response = await axios.get(url, {
-      params: { api_key: TMDB_API_KEY, query: query !== "popular" ? query : undefined },
+      params: { api_key: API_KEY, query: query !== "popular" ? query : undefined },
     });
-
     setMovies(response.data.results);
     setSelectedMovie(response.data.results[0]);
   };
 
-  const fetchTrailer = async (movie) => {
-    const response = await axios.get(`${TMDB_BASE_URL}/movie/${movie.id}/videos`, {
-      params: { api_key: TMDB_API_KEY },
+  const fetchTrailer = async (movieId) => {
+    const response = await axios.get(`${BASE_URL}/movie/${movieId}/videos`, {
+      params: { api_key: API_KEY },
     });
-
     const trailer = response.data.results.find((vid) => vid.type === "Trailer");
-
-    if (trailer) {
-      setTrailerId(trailer.key);
-    } else {
-      fetchYouTubeTrailer(movie.title);
-    }
-  };
-
-  const fetchYouTubeTrailer = async (movieTitle) => {
-    const response = await axios.get(YOUTUBE_SEARCH_URL, {
-      params: {
-        key: YOUTUBE_API_KEY,
-        q: `${movieTitle} trailer`,
-        part: "snippet",
-        maxResults: 1,
-        type: "video",
-      },
-    });
-
-    const youtubeTrailer = response.data.items[0];
-    setTrailerId(youtubeTrailer ? youtubeTrailer.id.videoId : "");
+    setTrailerId(trailer ? trailer.key : "");
   };
 
   const handleSearch = (event) => {
@@ -78,7 +54,7 @@ const App = () => {
         />
         <button type="submit">Search</button>
       </form>
-
+      
       {selectedMovie && (
         <div
           className="banner"
@@ -89,7 +65,7 @@ const App = () => {
           <div className="banner-content">
             <h1>{selectedMovie.title}</h1>
             <p>{selectedMovie.overview}</p>
-            <button onClick={() => fetchTrailer(selectedMovie)}>Play Trailer</button>
+            <button onClick={() => fetchTrailer(selectedMovie.id)}>Play Trailer</button>
           </div>
         </div>
       )}
